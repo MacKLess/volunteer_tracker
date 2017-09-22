d2ba1c944dcc10d7eeb0f82b3ce7c98e39d68d94
LD MacKrell <ldmackrell@gmail.com>
2017-09-22 11:38:12 -0700

    update app to save project classes.

--- lib/project.rb
+++ lib/project.rb
@@ -5,5 +5,24 @@ class Project
     @title = attributes.fetch(:title)
     @id = attributes.fetch(:id)
   end
-  
+
+  def self.all
+    project_list = DB.exec("SELECT * FROM projects;")
+    projects = []
+    project_list.each do |project|
+      title = project.fetch('title')
+      id = project.fetch('id').to_i
+      projects.push(Project.new({:title => title, :id => id}))
+    end
+    projects
+  end
+
+  def ==(another_project)
+    self.title().==(another_project.title()).&(self.id().==(another_project.id()))
+  end
+
+  def save
+    project = DB.exec("INSERT INTO projects (title) VALUES ('#{@title}') RETURNING id;")
+    @id = project.first.fetch('id').to_i
+  end
 end
