4fb3a11d889cc9271b1b7d10ece703c4e7ed128b
LD MacKrell <ldmackrell@gmail.com>
2017-09-22 11:56:20 -0700

    update app to find projects by their id.

--- lib/project.rb
+++ lib/project.rb
@@ -34,4 +34,9 @@ class Project
     end
   end
 
+  def update(attributes)
+    @title = attributes.fetch(:title, @title)
+    DB.exec("UPDATE projects SET title = '#{@title}' WHERE id = #{self.id};")
+  end
+  
 end
