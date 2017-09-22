2969f2f6b5d80c55dfba54117e57c581007af505
LD MacKrell <ldmackrell@gmail.com>
2017-09-22 15:52:57 -0700

    update app to update project title.

--- app.rb
+++ app.rb
@@ -20,3 +20,21 @@ post('/') do
   @projects = Project.all
   erb(:index)
 end
+
+get('/projects/:id') do
+  @project = Project.find(params.fetch(:id).to_i)
+  @volunteers = Volunteer.all
+  erb(:project_detail)
+end
+
+get('/project/:id/edit') do
+  @project = Project.find(params.fetch(:id).to_i)
+  erb(:project_update)
+end
+
+post('/project/:id/edit') do
+  @project = Project.find(params.fetch(:id).to_i)
+  title_change = params.fetch(:title)
+  @project.update({:title => title_change})
+  erb(:project_detail)
+end
